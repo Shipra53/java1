395908ff5f07338d80a116f108cf6b201973e872
Shipra53 <upadhyayshipra84@gmail.com>
2022-12-07 18:28:47 +0530

    my eleventh commit

---
+++ seventyone.java
@@ -0,0 +1,9 @@
+//to print the no. of elements present in an array
+import java.util.Scanner;
+public class seventyone{
+    public static void main(String[]args) {
+        Scanner input=new Scanner(System.in);
+        int [] a=new int[]{2,3,7,5,8,12};
+        System.out.println("Number of elements present in an array: "+a.length);
+    }
+}
