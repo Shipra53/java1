d6b58c2a3b6d2d96969f8aac0878374cb4313f97
Shipra53 <upadhyayshipra84@gmail.com>
2023-01-06 21:27:13 +0530

    my twenty-seventh commit

---
+++ seventysix.java
@@ -0,0 +1,16 @@
+//find third largest number in an array 
+import java.util.Scanner;
+public class seventysix{
+    public static void main(String[]args) {
+        Scanner input=new Scanner(System.in);
+        int [] a={2,3,7,5,8,12};
+        int temp=0;
+        for(int i=0;i<a.length;i++){
+            for(int j=i+1;j<a.length;j++){
+                if(a[i]>a[j]){
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }}}
+        System.out.println("THIRD LARGEST NUMBER: "+a[a.length-3]);
+    }}
